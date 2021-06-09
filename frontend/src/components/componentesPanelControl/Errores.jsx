8d909788e7d47e0e48d338586d34d6074e6142f1
Fernando Alvarado <50020544+Fernando-Alvarado@users.noreply.github.com>
2021-06-09 18:06:51 -0500

    cosas

---
+++ frontend/src/components/componentesPanelControl/Errores.jsx
@@ -0,0 +1,13 @@
+//archivos sin importar
+
+import React from 'react'
+
+const Errores = () => {
+    return (
+        <div>
+            <h1>Errores</h1>
+        </div>
+    )
+}
+
+export default Errores
---
+++ frontend/src/components/componentesPanelControl/LinksPasados.jsx
@@ -0,0 +1,13 @@
+//archivos sin importar
+
+import React from 'react'
+
+const LinksPasados = () => {
+    return (
+        <div>
+            <h1>links pasados</h1>
+        </div>
+    )
+}
+
+export default LinksPasados
